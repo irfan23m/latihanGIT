3ed9938fcc17997f840043b5d8d820a4644eb517
irfan23m <51937369+irfan23m@users.noreply.github.com>
2019-11-05 09:44:35 +0700

    java_project
    new project java

---
+++ belajar_git/src/belajar_git/MainClass.java
@@ -0,0 +1,8 @@
+package belajar_git;
+
+public class MainClass {
+	 public static void main(String[] args) {
+		System.out.println("This is a Version 1");
+		System.out.println("Hello World 1");
+	}
+}
